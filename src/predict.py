eead4ee5866bf2b4baa65b090856ab559ab5a622
Yiğitcan Kızıl <111859800+yigitcankzl@users.noreply.github.com>
2024-12-18 16:24:18 +0300

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,10 +1,11 @@
 import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
+import os
 
 categories = ["glioma", "meningioma", "notumor", "pituitary"]
 
-model = load_model('brain_tumor_detection_model.h5')
+model = load_model('src/model/brain_tumor_detection_model.h5')
 
 def prepare_image(img_path):
     img = image.load_img(img_path, target_size=(150, 150))  
